5ee5814a6a64e69b3f28f3b97934203969ff4d59
ToniMaunde <maundemilton@gmail.com>
2023-12-26 20:27:31 +0200

    feature: handle multi-value fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,61 @@
-import { z, type ZodSchema, type ZodError } from "zod";
+import { z, type ZodError, ZodObject, } from "zod";
 
 const ZProfileSchema = z.object({
   username: z.string(),
   email: z.string(),
   password: z.string(),
+  avatar: z.instanceof(File),
+  preferences: z.string().array(),
+  // preferences: z.string(),
 });
-
 type TProfile = z.infer<typeof ZProfileSchema>;
 
 function App() {
 
-  function castFormData<T>(formData: FormData, schema: ZodSchema): T | ZodError {
-    // Here the as T assertion is fine because Zod will validate whether the assertion is accurate
-    const result: T = Object.fromEntries(formData.entries()) as T;
+  type TRecord = Record<string, string | number | File | Array<string> | Array<number> | Array<File>>;
+  function castFormDataMultiValue<T extends TRecord>(formData: FormData, schema: ZodObject<any>): T | ZodError {
+
+    function dedupeFormDataKeys(formData: FormData) {
+      const keySet = new Set<string>();
+
+      formData.forEach((_, key) => {
+        keySet.add(key);
+      })
+
+      return [...keySet];
+    }
+
+    type TZodShapeDefTypeName = "ZodString" | "ZodEffects" | "ZodArray";
+    type TKeyType = "multi-value" | "single-value";
+    type TMappedFormDataKeys = Map<string, TKeyType>;
+    function mapKeys(formData: FormData): TMappedFormDataKeys {
+      const keyMap = new Map<string, TKeyType>();
+      const uniqueKeys = dedupeFormDataKeys(formData);
+
+      uniqueKeys.forEach(key => {
+        const shapeFieldType = schema.shape[key]._def.typeName as TZodShapeDefTypeName;
+        if (shapeFieldType === "ZodArray") keyMap.set(key, "multi-value");
+        else keyMap.set(key, "single-value");
+      });
+
+      return keyMap;
+    };
+
+
+    const profile = {} as TRecord;
+    const uniqueKeys = mapKeys(formData);
+
+    uniqueKeys.forEach((value, key) => {
+      if (value === "single-value") {
+        profile[key] = formData.get(key) as string | File;
+      } else {
+        profile[key] = formData.getAll(key) as Array<string> | Array<File>;
+      }
+    });
 
     try {
-      return schema.parse(result)
+      // @ts-ignore: still figuring out this line
+      return schema.parse(profile);
     } catch (error) {
       return error as ZodError;
     }
